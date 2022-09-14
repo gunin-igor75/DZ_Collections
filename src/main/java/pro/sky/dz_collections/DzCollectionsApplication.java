93c9714c48946337c4d66c4e7260a61383717f13
Igor Gunin <gunin-igor75@yandex.ru>
2022-09-14 21:36:35 +0600

    first commit

---
+++ src/main/java/pro/sky/dz_collections/DzCollectionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.dz_collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzCollectionsApplication.class, args);
+	}
+
+}
